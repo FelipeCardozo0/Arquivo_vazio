d81c6fb46bbb6c883a7f6ad5cda3781bb9858c8f
FelipeCardozo0 <focardo@emory.edu>
2024-10-04 21:38:11 -1200

    Create Lab5.java

---
+++ Lab5.java
@@ -0,0 +1,23 @@
+import javax.swing.plaf.IconUIResource;
+// Do we need package for input or main method is the input
+// Lower case and upper case are different
+
+public class Lab5 {
+    public static void main(String[] args) {
+        System.out.println(countCharacters("mississippi"));
+
+    }
+    public static String countCharacters(String s){
+        String alphabet  = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        s = s.replace (" ", "");
+
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if
+        }
+
+
+
+
+    }
+}
