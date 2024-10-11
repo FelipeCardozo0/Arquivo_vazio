d36b2c567f9fce0a74e971d2cf43ad18ae0d5523
FelipeCardozo0 <focardo@emory.edu>
2024-10-10 21:55:50 -1200

    Update Lab5.java

--- Lab5.java
+++ Lab5.java
@@ -1,4 +1,29 @@
-public class Main {
+// Java program for the above approach
+class Lab5 {
+    static void countCharacters(String str)
+    {
+        int count[] = new int[256];
+        for (int i = 0; i < str.length(); i++){
+            count[str.charAt(i)]++;
+        }
+
+        char ch[] = new char[str.length()];
+        for (int i = 0; i < str.length(); i++){
+            ch[i] = str.charAt(i);
+            int find = 0;
+            for (int j = 0; j <= i; j++) {
+
+                if (str.charAt(i) == ch[j]){
+                    find++;
+                }
+            }
+
+            if (find == 1) {
+                System.out.println(str.charAt(i) + ":" + count[str.charAt(i)]);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         String mississippi = "mississippi";
         String str = "!\"#$%&'()*+,-./0123456789:;ABCDEabcde ";
@@ -8,23 +33,5 @@ public class Main {
         countCharacters(str);
         System.out.println();
         countCharacters(str2);
-    }
-
-    public static void countCharacters(String s) {
-        String countedChars = "";
-        
-        for (int i = 0; i < s.length(); i++) {
-            char currentChar = s.charAt(i);
-            if (countedChars.indexOf(currentChar) == -1) { 
-                countedChars += currentChar;
-                int count = 0;
-                for (int j = 0; j < s.length(); j++) {
-                    if (s.charAt(j) == currentChar) {
-                        count++;
-                    }
-                }
-                System.out.println(currentChar + ": " + count);
-            }
-        }
     }
 }
