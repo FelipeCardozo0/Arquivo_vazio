f4a0dfc5e8bb9b885a17fff31fcd7c6c43df3df8
FelipeCardozo0 <focardo@emory.edu>
2024-10-10 21:53:51 -1200

    Update Lab5.java

--- Lab5.java
+++ Lab5.java
@@ -1,68 +1,30 @@
-public class Lab5 {
+public class Main {
     public static void main(String[] args) {
-        // Call the method and print the result
-        String result = countCharacters("O botafogo Vai ser campeao da libertadores");
-        System.out.println(result);
+        String mississippi = "mississippi";
+        String str = "!\"#$%&'()*+,-./0123456789:;ABCDEabcde ";
+        String str2 = "!\"#$%&'()*+,-./0123456789:;ABCDEabcde !\"#$%&'()*+,-./0123456789:;ABCDEabcde !\"#$%&'()*+,-./0123456789:;ABCDEabcde ";
+        countCharacters(mississippi);
+        System.out.println();
+        countCharacters(str);
+        System.out.println();
+        countCharacters(str2);
     }
 
-    public static String countCharacters(String s) {
-        StringBuilder result = new StringBuilder(); // To store the counts as a string
-
-        // Iterate over each character in the string
+    public static void countCharacters(String s) {
+        String countedChars = "";
+        
         for (int i = 0; i < s.length(); i++) {
             char currentChar = s.charAt(i);
-            int count = 0;
-
-            // Count occurrences of currentChar in the string
-            for (int j = 0; j < s.length(); j++) {
-                if (s.charAt(j) == currentChar) {
-                    count++;
+            if (countedChars.indexOf(currentChar) == -1) { 
+                countedChars += currentChar;
+                int count = 0;
+                for (int j = 0; j < s.length(); j++) {
+                    if (s.charAt(j) == currentChar) {
+                        count++;
+                    }
                 }
-            }
-
-            // Append the character and its count if it's the first occurrence in the loop
-            if (s.indexOf(currentChar) == i) {
-                result.append(currentChar).append(" : ").append(count).append("\n");
-            }
-        }
-
-        // Return the result as a string
-        return result.toString();
-    }
-}
-
-
-/*
-import javax.swing.plaf.IconUIResource;
-// Do we need package for input or main method is the input
-// Do we need to count spaces
-
-public class Lab5 {
-    public static void main(String[] args) {
-        System.out.println(countCharacters("mississippi"));
-
-
-    }
-    public static String countCharacters(String s) {
-        // Iterate over each character in the string
-        for (int i = 0; i < s.length(); i++) {
-            char currentChar = s.charAt(i);
-            int count = 0;
-
-            // Count occurrences of currentChar in the string
-            for (int j = 0; j < s.length(); j++) {
-                if (s.charAt(j) == currentChar) {
-                    count++;
-                }
-            }
-
-            // Print the character and its count if it's the first occurrence in the loop
-            if (s.indexOf(currentChar) == i) {
-                System.out.println(currentChar + " : " + count);
+                System.out.println(currentChar + ": " + count);
             }
         }
     }
 }
-
- */
-
